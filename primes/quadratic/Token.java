f52b6c6300fa40b091eda913d1ef383550be4e4d
Marco Pedicini <marco@iac.cnr.it>
2018-04-18 19:12:27 +0200

    added Token class to manage factoring with the quadratic sieve

---
+++ primes/quadratic/Token.java
@@ -0,0 +1,67 @@
+/*
+ * This new variant of the Token class has to manage
+ * residuals of the factoring integer
+ * @author MP
+ */
+
+package primes.quadratic ;
+
+import java.math.BigInteger ;
+import primes.erathostenes.Sieve ;
+import primes.erathostenes.Item ;
+
+class Token extends primes.erathostenes.Token {
+//private BigInteger num ;
+private Biginteger residual ;
+private Item row ;
+private boolean primality ;
+
+	static final BigInteger TWO = new BigInteger("2");
+	
+	//costruttori
+	 Token() {
+		this.Set(BigInteger.ZERO) ;
+		
+	}
+
+	// setters
+/*	void Set(BigInteger n) {
+		
+		this.num = n ;
+		
+		
+	}
+*/
+	
+	public void Set(Sieve sieve,BigInteger n) {
+		this.Set(n) ;
+		this.top = sieve.next() ;
+		this.parity = ((!(n.compareTo(Token.TWO)==0))&& (n.mod(Token.TWO).compareTo(BigInteger.ZERO) == 0))  ;
+	}
+	
+	void SetTop(Item link) {
+		this.top = link ;
+		
+	}
+	
+	
+	// getters
+/*	BigInteger value() {
+		
+		return this.num;
+	}
+*/
+	
+	Item top() {
+		
+		return this.top;
+	}
+	
+	boolean parity() {
+		
+		return this.parity;
+	}
+
+
+
+}
