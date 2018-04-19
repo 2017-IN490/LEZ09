9c6f212b77ff96da386c5230c86247c4c73f4275
Marco Pedicini <marco@iac.cnr.it>
2018-04-19 14:01:44 +0200

    primi cambiamenti : HTML nei commenti

--- primes/quadratic/Matrix.java
+++ primes/quadratic/Matrix.java
@@ -13,9 +13,10 @@ class Matrix extends Item {
 	BigInteger entry ;
 	Item nextrow ;
 
-    /* Costruttore che inserisce un nuovo elemento e
-       nella matrice  che continua con la riga n
-       e la colonna nrow:
+    /**
+	   Costruttore che inserisce un nuovo elemento e
+       nella matrice  che continua con la riga <em>n</em>
+       e la colonna <em>nrow</em>:
        @param n argomento di tipo Item che punta al resto della riga
        @param nrow argomento di tipo Item che punta al resto della colonna
        @param e il valore intero da inserire nella matrice
@@ -25,21 +26,22 @@ class Matrix extends Item {
 		super(n) ;
 		this.set(nrow, e);
 	}
-       
-    /* Metodo setter che prende 
+	
+/** Metodo setter che prende
        @param nrow la prossima riga della matrice 
        @param e    valore da immettere
-    */
+*/
 	void set(Item nrow, BigInteger e) {
 		this.nextrow = nrow;
 		this.entry = e;
 	}
 	// getters
 	
-    /* Implementazione del Metodo get ereditato
+/**
+	 Implementazione del Metodo get ereditato
        calcolo del rango della matrice (da fare)
        @return restituisce l'intero nel token
-     */
+*/
 public    Token get() {
 	return null ;
 }
